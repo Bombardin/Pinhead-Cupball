de319d8c28ac1051fb790146c548e76d44554247
Zuola Guoerluoti <zguoerlu@ucsc.edu>
2018-11-13 22:39:17 -0800

    Newboard, stringPull, new layout

---
+++ Pinhead/Assets/Scripts/BallScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+
+    private int count { get; set; }
+    private bool nextState {get; set;}
+    private Rigidbody rb;
+    public Text countText;
+    public Text loseText;
+    public Text scoreText;
+    private int score;
+    Vector3 originalPosition;
+    //private Vector3 startingPosition = rb.Transform.localPosition();
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        count = 3;
+        score = 0;
+        countText.text = "BallCount: " + count.ToString();
+        loseText.text = "";
+        scoreText.text = "Score: " + score.ToString();
+        originalPosition = 
+            new Vector3(rb.gameObject.transform.position.x, 
+                        rb.gameObject.transform.position.y, 
+                        rb.gameObject.transform.position.z);
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Catcher"))
+        {
+            count = count - 1;
+            countText.text = "BallCount: " + count.ToString();
+            if (count <= 0)
+            {
+                loseText.text = "Game Over";
+            }
+            else
+            {
+                transform.position = originalPosition;
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Bumper"))
+        {
+            score += 100;
+            scoreText.text = "Score: " + score.ToString();
+        }
+    }
+
+}
---
+++ Pinhead/Assets/Scripts/PullString.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PullString : MonoBehaviour {
+    public string inputName;
+    public float distance = 50.0f;
+    public float speed = 1.0f;
+    public GameObject ball;
+    public float power = 2000.0f;
+    private bool ready { get; set; }
+    private bool fire { get; set; }
+    private float moveCount { get; set; }
+    
+    // Use this for initialization
+    void Start () {
+        ready = false;
+        fire = false;
+        moveCount = 0.0f;
+	}
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            ready = true;
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetButton(inputName))
+        //gradually pull down as button is held
+        { 
+            if(moveCount < distance)
+            {
+                transform.Translate(0, 0, -speed * Time.deltaTime);
+                moveCount += speed * Time.deltaTime;
+                fire = true;
+            }
+        }
+        else if(moveCount > 0)
+        {
+            if(fire && ready)
+            {
+                //shoot the ball
+                ball.transform.TransformDirection(Vector3.forward * 10);
+                ball.GetComponent<Rigidbody>().AddForce(0, 0, moveCount * power);
+                fire = false;
+                ready = false;
+            }
+            //fire 
+            transform.Translate(0, 0, 20 * Time.deltaTime);
+            moveCount -= 20 * Time.deltaTime;
+        }
+        //to stay on the board
+        if(moveCount <= 0)
+        {
+            fire = false;
+            moveCount = 0;
+        }
+	}
+    
+}
